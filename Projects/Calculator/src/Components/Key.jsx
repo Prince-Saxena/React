4c6eb89fb3937e2c161adc60a7bdd9b14582f008
Prince-Saxena <princesaxena202020@gmail.com>
2024-08-15 10:29:02 +0530

    Remove all files from Git repository

--- Projects/Calculator/src/Components/Key.jsx
+++
@@ -1,7 +0,0 @@
-import PropTypes from "prop-types";
-export default function Key({ itm }) {
-  return <button className="col-2 m-2 btn border-primary ">{itm}</button>;
-}
-Key.propTypes = {
-  itm: PropTypes.string.isRequired, // Ensure itm is a required string
-};
--- Projects/ContextAPI/src/components/Profile.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UserContext from "./UserContext";
-import { useContext } from "react";
-
-export default function Profile() {
-  const { user } = useContext(UserContext);
-  return <div>Welcome : {user}</div>;
-}
